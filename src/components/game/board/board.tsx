193b17f2c8ccb6bfdc85da2f48de38f0746547ff
Nadine <nadlgit@users.noreply.github.com>
2023-04-09 18:15:43 -0400

    feat: counter drop animation

--- src/components/game/board/board.tsx
+++ src/components/game/board/board.tsx
@@ -5,18 +5,29 @@ type BoardProps = {};
 export const Board = ({}: BoardProps) => {
   //prettier-ignore
   const grid = [
-    'R', 'Y', '', '', '', '', 'Y',
-    'R', 'Y', '', '', '', '', 'R',
-    'R', 'Y', '', '', '', '', 'R',
-    'R', 'Y', '', '', '', '', 'Y',
-    'R', 'Y', '', '', '', '', 'R',
-    'R', 'Y', '', '', '', '', 'Y'
+    undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+    undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+    undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+    undefined, undefined, undefined, undefined, undefined, undefined, undefined,
+    { color: 'R', dropOffset: 5 }, undefined, undefined, undefined, undefined, undefined, undefined,
+    { color: 'Y' }, undefined, undefined, undefined, undefined, undefined, undefined
   ];
   return (
     <div className={styles.grid}>
-      {grid.map((cell) => (
-        <div className={styles.cell} data-content={cell} />
-      ))}
+      {grid.map((cell) => {
+        const cssClasses = [styles.cell];
+        let inlineStyle = {};
+        if (cell?.dropOffset !== undefined) {
+          cssClasses.push(styles.dropping);
+          inlineStyle = {
+            '--drop-offset': cell.dropOffset,
+            '--drop-duration': `${0.2 * cell.dropOffset}s`,
+          };
+        }
+        return (
+          <div className={cssClasses.join(' ')} data-content={cell?.color} style={inlineStyle} />
+        );
+      })}
     </div>
   );
 };
