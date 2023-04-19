80e238e4e52f4b2136d3bf0b061f7a10acbbfb31
Nadine <nadlgit@users.noreply.github.com>
2023-04-19 19:53:49 -0400

    feat: play() victory

--- src/core/test/game-play.test.ts
+++ src/core/test/game-play.test.ts
@@ -10,7 +10,7 @@ describe('play()', () => {
     const game = new Game(undefined, undefined, initialState);
     game.play('RED', 2);
     const gameState = game.getState();
-    expect(gameState.boardCounters).toStrictEqual([{ row: 1, column: 2, color: 'RED' }]);
+    expect(gameState.boardCounters).toIncludeSameMembers([{ row: 1, column: 2, color: 'RED' }]);
     expect(gameState.droppedCounter).toStrictEqual({ row: 1, column: 2 });
   });
 
@@ -25,7 +25,7 @@ describe('play()', () => {
     const game = new Game(undefined, undefined, initialState);
     game.play('RED', 1);
     const gameState = game.getState();
-    expect(gameState.boardCounters).toStrictEqual([
+    expect(gameState.boardCounters).toIncludeSameMembers([
       { row: 1, column: 1, color: 'RED' },
       { row: 2, column: 1, color: 'YELLOW' },
       { row: 3, column: 1, color: 'RED' },
@@ -49,7 +49,7 @@ describe('play()', () => {
     const game = new Game(undefined, undefined, initialState);
     game.play('RED', 1);
     const gameState = game.getState();
-    expect(gameState.boardCounters).toStrictEqual([
+    expect(gameState.boardCounters).toIncludeSameMembers([
       { row: 1, column: 1, color: 'RED' },
       { row: 2, column: 1, color: 'YELLOW' },
       { row: 3, column: 1, color: 'RED' },
@@ -69,7 +69,7 @@ describe('play()', () => {
     const game = new Game(undefined, undefined, initialState);
     game.play('RED', 2);
     const gameState = game.getState();
-    expect(gameState.boardCounters).toStrictEqual([{ row: 1, column: 1, color: 'RED' }]);
+    expect(gameState.boardCounters).toIncludeSameMembers([{ row: 1, column: 1, color: 'RED' }]);
     expect(gameState.droppedCounter).toBeUndefined();
   });
 
@@ -83,5 +83,207 @@ describe('play()', () => {
     const gameState = game.getState();
     expect(gameState.players.RED.isCurrentPlayer).toBe(false);
     expect(gameState.players.YELLOW.isCurrentPlayer).toBe(true);
+  });
+
+  describe('given victory', () => {
+    test('detects horizontal pattern', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 2, column: 1, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 1, column: 3, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.boardCounters).toIncludeSameMembers([
+        { row: 1, column: 1, color: 'RED', isWinPart: true },
+        { row: 2, column: 1, color: 'YELLOW' },
+        { row: 1, column: 2, color: 'RED', isWinPart: true },
+        { row: 2, column: 2, color: 'YELLOW' },
+        { row: 1, column: 3, color: 'RED', isWinPart: true },
+        { row: 2, column: 3, color: 'YELLOW' },
+        { row: 1, column: 4, color: 'RED', isWinPart: true },
+      ]);
+    });
+
+    test('detects vertical pattern', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 1, column: 2, color: 'YELLOW' },
+          { row: 2, column: 1, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 3, column: 1, color: 'RED' },
+          { row: 3, column: 2, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 1);
+      const gameState = game.getState();
+      expect(gameState.boardCounters).toIncludeSameMembers([
+        { row: 1, column: 1, color: 'RED', isWinPart: true },
+        { row: 1, column: 2, color: 'YELLOW' },
+        { row: 2, column: 1, color: 'RED', isWinPart: true },
+        { row: 2, column: 2, color: 'YELLOW' },
+        { row: 3, column: 1, color: 'RED', isWinPart: true },
+        { row: 3, column: 2, color: 'YELLOW' },
+        { row: 4, column: 1, color: 'RED', isWinPart: true },
+      ]);
+    });
+
+    test('detects ascending diagonal pattern', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 1, column: 2, color: 'YELLOW' },
+          { row: 2, column: 2, color: 'RED' },
+          { row: 1, column: 3, color: 'YELLOW' },
+          { row: 1, column: 4, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+          { row: 3, column: 3, color: 'RED' },
+          { row: 2, column: 4, color: 'YELLOW' },
+          { row: 1, column: 5, color: 'RED' },
+          { row: 3, column: 4, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.boardCounters).toIncludeSameMembers([
+        { row: 1, column: 1, color: 'RED', isWinPart: true },
+        { row: 1, column: 2, color: 'YELLOW' },
+        { row: 2, column: 2, color: 'RED', isWinPart: true },
+        { row: 1, column: 3, color: 'YELLOW' },
+        { row: 1, column: 4, color: 'RED' },
+        { row: 2, column: 3, color: 'YELLOW' },
+        { row: 3, column: 3, color: 'RED', isWinPart: true },
+        { row: 2, column: 4, color: 'YELLOW' },
+        { row: 1, column: 5, color: 'RED' },
+        { row: 3, column: 4, color: 'YELLOW' },
+        { row: 4, column: 4, color: 'RED', isWinPart: true },
+      ]);
+    });
+
+    test('detects descending diagonal pattern', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 4, color: 'RED' },
+          { row: 1, column: 3, color: 'YELLOW' },
+          { row: 2, column: 3, color: 'RED' },
+          { row: 1, column: 5, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 3, column: 2, color: 'RED' },
+          { row: 1, column: 1, color: 'YELLOW' },
+          { row: 2, column: 1, color: 'RED' },
+          { row: 3, column: 1, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 1);
+      const gameState = game.getState();
+      expect(gameState.boardCounters).toIncludeSameMembers([
+        { row: 1, column: 4, color: 'RED', isWinPart: true },
+        { row: 1, column: 3, color: 'YELLOW' },
+        { row: 2, column: 3, color: 'RED', isWinPart: true },
+        { row: 1, column: 5, color: 'YELLOW' },
+        { row: 1, column: 2, color: 'RED' },
+        { row: 2, column: 2, color: 'YELLOW' },
+        { row: 3, column: 2, color: 'RED', isWinPart: true },
+        { row: 1, column: 1, color: 'YELLOW' },
+        { row: 2, column: 1, color: 'RED' },
+        { row: 3, column: 1, color: 'YELLOW' },
+        { row: 4, column: 1, color: 'RED', isWinPart: true },
+      ]);
+    });
+
+    test('sets winner', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 2, column: 1, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 1, column: 3, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .withWinner(null)
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.players.RED.isWinner).toBe(true);
+      expect(gameState.players.YELLOW.isWinner).toBe(false);
+    });
+
+    test('updates winner score', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 2, column: 1, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 1, column: 3, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .withRedPlayerScore(1)
+        .withYellowPlayerScore(1)
+        .withUpdatedPlayerScore(undefined)
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.players.RED.score).toBe(2);
+      expect(gameState.players.YELLOW.score).toBe(1);
+      expect(gameState.updatedPlayerScore).toBe('RED');
+    });
+
+    test('ends round', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 2, column: 1, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 1, column: 3, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.isRoundEnded).toBe(true);
+    });
+
+    test('sets current player to none', () => {
+      const initialState = new GameStateBuilder()
+        .withBoardCounters([
+          { row: 1, column: 1, color: 'RED' },
+          { row: 2, column: 1, color: 'YELLOW' },
+          { row: 1, column: 2, color: 'RED' },
+          { row: 2, column: 2, color: 'YELLOW' },
+          { row: 1, column: 3, color: 'RED' },
+          { row: 2, column: 3, color: 'YELLOW' },
+        ])
+        .withCurrentPlayer('RED')
+        .build();
+      const game = new Game(undefined, undefined, initialState);
+      game.play('RED', 4);
+      const gameState = game.getState();
+      expect(gameState.players.RED.isCurrentPlayer).toBe(false);
+      expect(gameState.players.YELLOW.isCurrentPlayer).toBe(false);
+    });
   });
 });
