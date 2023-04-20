6dc109dc0f6af8cd00ca07be56af476820c75cbb
Nadine <nadlgit@users.noreply.github.com>
2023-04-20 08:52:40 -0400

    feat: Score component using context

--- src/components/game/score/score.tsx
+++ src/components/game/score/score.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { type PlayerColor } from '@/core';
+import { useGame } from '@/store';
 import styles from './score.module.css';
 import imgPlayer1 from './player-one.svg';
 import imgPlayer2 from './player-two.svg';
@@ -9,21 +11,29 @@ type ScoreProps = {
   player: PlayerColor;
 };
 
-const scoreConfig: Record<
-  PlayerColor,
-  { value: number; label: string; icon: string; position: 'left' | 'right' }
-> = {
-  RED: { value: 12, label: 'Player 1', icon: imgPlayer1, position: 'left' },
-  YELLOW: { value: 23, label: 'Player 2', icon: imgPlayer2, position: 'right' },
-};
-
 export const Score = ({ player }: ScoreProps) => {
-  const { value, label, icon, position } = scoreConfig[player];
+  const { playMode, playerNames, playerScores } = useGame();
+  const [score, setScore] = useState(playerScores[player]);
+  // const [prevScore, setPrevScore] = useState(playerScores[player]);
+  const position = player === 'RED' ? 'left' : player === 'YELLOW' ? 'right' : '';
+  const icon = (() => {
+    if (player === 'RED' && playMode === 'vsPlayer') return imgPlayer1;
+    if (player === 'RED' && playMode === 'vsCPU') return imgYou;
+    if (player === 'YELLOW' && playMode === 'vsPlayer') return imgPlayer2;
+    if (player === 'YELLOW' && playMode === 'vsCPU') return imgCPU;
+    return '';
+  })();
+
+  useEffect(() => {
+    // setPrevScore(score);
+    setScore(playerScores[player]);
+  }, [playerScores[player]]);
+
   return (
     <div className={styles.container} data-position={position}>
       <img src={icon} alt="" className={styles.icon} />
-      <span className={styles.label}>{label}</span>
-      <span className={styles.score}>{value}</span>
+      <span className={styles.label}>{playerNames[player]}</span>
+      <span className={styles.score}>{score}</span>
     </div>
   );
 };
