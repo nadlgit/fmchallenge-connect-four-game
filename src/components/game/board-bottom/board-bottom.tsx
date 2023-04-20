b0a94ed79a76ef54c576512fd5bbe62893cdb274
Nadine <nadlgit@users.noreply.github.com>
2023-04-20 17:29:58 -0400

    refactor: BoardBottom component

--- src/components/game/board-bottom/board-bottom.tsx
+++ src/components/game/board-bottom/board-bottom.tsx
@@ -1,41 +1,44 @@
-import { useEffect, useState } from 'react';
 import { useGame } from '@/store';
 import styles from './board-bottom.module.css';
 
 export const BoardBottom = () => {
   const { currentPlayer, winner, startNewRound } = useGame();
-  const [timerLabel, setTimerLabel] = useState('');
 
+  const timerLabel = `${currentPlayer.name}'s turn`;
   const timerValue = 30;
-  const verbLabel = `win${winner.name.toLowerCase() === 'you' ? '' : 's'}`;
+  const winnerResult = winner.color
+    ? `win${winner.name.toLowerCase() === 'you' ? '' : 's'}`
+    : 'tie';
 
-  useEffect(() => {
-    if (currentPlayer.name) {
-      setTimerLabel(`${currentPlayer.name}'s turn`);
-    }
-  }, [currentPlayer.name]);
+  const timerElement = (
+    <div className={styles.timer} data-color={currentPlayer.color}>
+      <span className={styles.timerlabel}>{timerLabel}</span>
+      <span className={styles.timervalue}>{`${timerValue}s`}</span>
+    </div>
+  );
+
+  const winnerElement = (
+    <div className={styles.winner}>
+      <span className={styles.winnerplayer}>{winner.name}</span>
+      <span className={styles.winnerresult}>{winnerResult}</span>
+      <button className={styles.winnerbutton} onClick={startNewRound}>
+        Play again
+      </button>
+    </div>
+  );
 
   return (
     <div
-      className={`${styles.container} ${
+      className={`${styles.container}  ${
         winner.color === 'RED'
           ? styles.bottomred
           : winner.color === 'YELLOW'
           ? styles.bottomyellow
           : ''
-      }`}
+      } ${winner.color || !currentPlayer.color ? styles.winnervisible : ''}`}
     >
-      <div className={styles.timer} data-color={(currentPlayer.color ?? '').substring(0, 1)}>
-        <span className={styles.label}>{timerLabel}</span>
-        <span className={styles.value}>{`${timerValue}s`}</span>
-      </div>
-      <div className={`${styles.winner} ${winner.color ? styles.endgame : ''}`}>
-        <span className={styles.player}>{winner.name}</span>
-        <span className={styles.wins}>{verbLabel}</span>
-        <button className={styles.button} onClick={startNewRound}>
-          Play again
-        </button>
-      </div>
+      {timerElement}
+      {winnerElement}
     </div>
   );
 };
