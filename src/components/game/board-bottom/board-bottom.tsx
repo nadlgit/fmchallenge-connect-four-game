422af6bb682ef82c4ee1e2abfbc9cbf513fec75a
Nadine <nadlgit@users.noreply.github.com>
2023-04-20 10:10:16 -0400

    feat: BoardBottom component using context

--- src/components/game/board-bottom/board-bottom.tsx
+++ src/components/game/board-bottom/board-bottom.tsx
@@ -1,29 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useGame } from '@/store';
 import styles from './board-bottom.module.css';
 
-type BoardBottomProps = {};
+export const BoardBottom = () => {
+  const { currentPlayer, winner, startNewRound } = useGame();
+  const [timerLabel, setTimerLabel] = useState('');
 
-export const BoardBottom = ({}: BoardBottomProps) => {
-  const timerLabel = "Player 1's turn";
   const timerValue = 30;
+  const verbLabel = `win${winner.name.toLowerCase() === 'you' ? '' : 's'}`;
 
-  // <div
-  //   className={`${styles.container} ${
-  //     winner === 'R' ? styles.bottomred : winner === 'Y' ? styles.bottomyellow : ''
-  //   }`}
-  // ></div>
+  useEffect(() => {
+    if (currentPlayer.name) {
+      setTimerLabel(`${currentPlayer.name}'s turn`);
+    }
+  }, [currentPlayer.name]);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.timer} data-color="R">
+    <div
+      className={`${styles.container} ${
+        winner.color === 'RED'
+          ? styles.bottomred
+          : winner.color === 'YELLOW'
+          ? styles.bottomyellow
+          : ''
+      }`}
+    >
+      <div className={styles.timer} data-color={(currentPlayer.color ?? '').substring(0, 1)}>
         <span className={styles.label}>{timerLabel}</span>
         <span className={styles.value}>{`${timerValue}s`}</span>
       </div>
-      {/* <div className={`${styles.winner} ${winner ? styles.endgame : ''}`}>
-        <span className={styles.player}>{winnerLabel}</span>
+      <div className={`${styles.winner} ${winner.color ? styles.endgame : ''}`}>
+        <span className={styles.player}>{winner.name}</span>
         <span className={styles.wins}>{verbLabel}</span>
-        <button className={styles.button} onClick={onPlayAgain}>
+        <button className={styles.button} onClick={startNewRound}>
           Play again
         </button>
-      </div> */}
+      </div>
     </div>
   );
 };
